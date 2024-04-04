0f6dd7ffe01fc724bc856bb6fad0a6dcd051652d
mkhalifa187 <158192194+mkhalifa187@users.noreply.github.com>
2024-04-04 21:05:51 +0100

    Update index.jsx

--- src/scenes/loginform/index.jsx
+++ src/scenes/loginform/index.jsx
@@ -1,17 +1,126 @@
-import { Box, Typography, useTheme } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-import { tokens } from "../../theme";
-import { mockDataUsers } from "../../data/mockData";
-import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import { Box, Button, TextField } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
+const initialValues = {
+    UserName: "",
+    Password: "",
+    retypepassword: "",
+    
+};
 
-			<form action="login.php" method="post">
-				<label>Username:</label>
-				<input type="text" name="Username"><br><br>
-				<label>Password:</label>
-				<input type="Password" name="Password"><br><br>
-				<input type="submit" name="submit" value="Login">
-			</form>
+const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const userSchema = yup.object().shape({
+    UserName: yup.string().required("required"),
+    Password: yup.string().required("required"),
+    retypepassword: yup.string().required("required"),
+    
+})
+
+const Form = () => {
+    const isNonMobile = useMediaQuery("(min-width:600px)");
+
+    const handleFormSubmit = (values) => {
+        console.log(values);
+    }
+
+    return <Box m="20px">
+        <Header title="Login" subtitle="login to your account" />
+
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValues}
+          validationSchema={userSchema}
+        >
+            {({ values, errors, touched, handleBlur, handleChange, handleSubmit}) => (
+                <form onSubmit={handleSubmit}>
+                    <Box 
+                        display="grid" 
+                        gap="30px" 
+                        gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                        sx={{
+                            "& > div": { gridColumn: isNonMobile ? undefined : "span 4"},
+                        }}
+                    >
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="First Name"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.firstName}
+                            name="firstName"
+                            error={!!touched.firstName && !!errors.firstName}
+                            helperText={touched.firstName && errors.firstName}
+                            sx={{ gridColumn: "span 2"}}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="Last Name"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.lastName}
+                            name="lastName"
+                            error={!!touched.lastName && !!errors.lastName}
+                            helperText={touched.lastName && errors.lastName}
+                            sx={{ gridColumn: "span 2"}}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="Email"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.email}
+                            name="email"
+                            error={!!touched.email && !!errors.email}
+                            helperText={touched.email && errors.email}
+                            sx={{ gridColumn: "span 4"}}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="Contact Number"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.contact}
+                            name="contact"
+                            error={!!touched.contact && !!errors.contact}
+                            helperText={touched.contact && errors.contact}
+                            sx={{ gridColumn: "span 4"}}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="Age"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.age}
+                            name="age"
+                            error={!!touched.age && !!errors.age}
+                            helperText={touched.age && errors.age}
+                            sx={{ gridColumn: "span 4"}}
+                        />
+                    </Box>
+                    <Box display="flex" justifyContent="end" mt="20px">
+                        <Button type="submit" color="secondary" variant="contained">
+                            login
+                        </Button>
+                    </Box>
+                </form>
+            )}
+
+        </Formik>
+    </Box>;
+};
+
+export default Form;
