008627452a1d6b60ee4ec87b1749e04d0303f151
Suhxyb <suhayb.ahmed2001@gmail.com>
2024-04-10 10:42:27 +0100

    updated with functions to fetch data from database

---
+++ src/Backend/DatabaseConnection.js
@@ -0,0 +1,87 @@
+// Importing MySQL module
+const mysql = require('mysql');
+
+// Creates a connection to the database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'Rakusens'
+});
+
+// Function to fetch users from the database
+function fetchUsers(callback) {
+    const query = 'SELECT * FROM Users';
+
+    connection.query(query, (error, results) => {
+        if (error) {
+            console.error('Error fetching users:', error);
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    });
+}
+
+// Function to fetch purchase orders from the database
+function fetchPurchaseOrders(callback) {
+    const query = 'SELECT * FROM PurchaseOrders';
+
+    connection.query(query, (error, results) => {
+        if (error) {
+            console.error('Error fetching purchase orders:', error);
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    });
+}
+
+// Function to fetch vendors from the database
+function fetchVendors(callback) {
+    const query = 'SELECT * FROM Vendors';
+
+    connection.query(query, (error, results) => {
+        if (error) {
+            console.error('Error fetching vendors:', error);
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    });
+}
+
+// Function to fetch products from the database
+function fetchProducts(callback) {
+    const query = 'SELECT * FROM Products';
+
+    connection.query(query, (error, results) => {
+        if (error) {
+            console.error('Error fetching products:', error);
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    });
+}
+
+// Function to fetch ingredients from the database
+function fetchIngredients(callback) {
+    const query = 'SELECT * FROM Ingredients';
+
+    connection.query(query, (error, results) => {
+        if (error) {
+            console.error('Error fetching ingredients:', error);
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    });
+}
+
+// Exporting the fetchUsers function
+module.exports.fetchUsers = fetchUsers;
+module.exports.fetchPurchaseOrders = fetchPurchaseOrders;
+module.exports.fetchVendors = fetchVendors;
+module.exports.fetchProducts = fetchProducts;
+module.exports.fetchIngredients = fetchIngredients;
