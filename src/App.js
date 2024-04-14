a59eb73af600f5247a6fe4de0833fab0dd6728c1
mkhalifa187 <158192194+mkhalifa187@users.noreply.github.com>
2024-04-14 15:29:07 +0100

    updated so login is required to enter program

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
+import { Box, Button, CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
-import Topbar from "./scenes/global/Topbar"
+import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Users from "./scenes/users";
@@ -10,42 +10,51 @@ import Form from "./scenes/form";
 import Applications from "./scenes/applications";
 import Orders from "./scenes/orders";
 import FAQ from "./scenes/faq";
-
 import Products from "./scenes/products";
 import Ingredients from "./scenes/ingredients";
-
 import Bar from "./scenes/bar";
 import Pie from "./scenes/pie";
 import Line from "./scenes/line";
+import LoginForm from "./scenes/Login";
 
 function App() {
   const [theme, colorMode] = useMode();
   const [notifications, setNotifications] = useState([]); // Define notifications state
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // Your login logic goes here, for simplicity, let's just set loggedIn to true
+    setLoggedIn(true);
+  };
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar />
-          <main className="content">
-          <Topbar setNotifications={setNotifications} /> {/* Pass setNotifications as a prop */}
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/applications" element={<Applications />} />
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/faq" element={<FAQ />} />
-
-              <Route path="/products" element={<Products />} />
-              <Route path="/ingredients" element={<Ingredients />} />
-
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-            </Routes>
-          </main>          
+          {loggedIn ? (
+            <>
+              <Sidebar />
+              <main className="content">
+                <Topbar setNotifications={setNotifications} />
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/users" element={<Users />} />
+                  <Route path="/form" element={<Form />} />
+                  <Route path="/applications" element={<Applications />} />
+                  <Route path="/orders" element={<Orders />} />
+                  <Route path="/faq" element={<FAQ />} />
+                  <Route path="/products" element={<Products />} />
+                  <Route path="/ingredients" element={<Ingredients />} />
+                  <Route path="/bar" element={<Bar />} />
+                  <Route path="/pie" element={<Pie />} />
+                  <Route path="/line" element={<Line />} />
+                </Routes>
+              </main>
+            </>
+          ) : (
+            <LoginForm onLogin={handleLogin} />
+          )}
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
